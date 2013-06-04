fc229957f021c38ae8632100e709b99f9a9d9b41
derwilli <theo@ubuntu.(none)>
2013-06-04 18:31:43 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileupload::Application.config.secret_token = 'dc858f4fb966929ee3cfe421fbd4640c83a4a74d50e8f89066abb2a92cd84360e212db8979614c4cf263470dcef6b146b4500e44370500dc0deee61708da8fd4'
